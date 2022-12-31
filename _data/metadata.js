ebfd1965587c5e0f10f54c2968543bc9a43ee639
Sam Kapila <sameerakapila@gmail.com>
2022-12-31 15:23:57 -0600

    tweetback

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 let data = {
-	username: "eleven_ty", // No leading @ here
-	homeLabel: "11ty.dev",
-	homeUrl: "https://www.11ty.dev/",
+	username: "samkap", // No leading @ here
+	homeLabel: "samkapila.com",
+	homeUrl: "https://www.samkapila.com",
 };
 
 data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
